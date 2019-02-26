3f3f40d9020843c9890de2d0794238119aae9820
Faysal Ahamed <miufaysal@live.com>
2019-02-26 12:57:07 +0600

    footer updated

--- footer.php
+++ footer.php
@@ -1,33 +1,20 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package BURO
- */
-
-?>
-
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'buro' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'buro' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'buro' ), 'buro', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer>
+    	<div class="fluid-container">
+        	<div class="content-row">
+            	<div class="grid-8">
+                	<p class="text-medium white ultralight">Copyright  © 2018, CHRD, All Rights Reserved</p>
+                </div>
+                <div class="grid-4 text-right">
+                	<div class="social-icons">
+                    	<a href=""><span class="icon-facebook"></span></a>
+                        <a href=""><span class="icon-twitter"></span></a>
+                        <a href=""><span class="icon-linkedin"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
