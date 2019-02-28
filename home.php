1fbc619dd3e8d427030c1e503986b58ac13ce367
Faysal Ahamed <miufaysal@live.com>
2019-02-28 17:27:22 +0600

    Home page template

---
+++ home.php
@@ -0,0 +1,118 @@
+<?php get_header(); ?>
+	<section class="banner clear-fixed">
+    	<div class="slider-border"></div>
+    	<div class="fluid-container">
+        	<div class="content-row">
+            	<div class="grid-12">
+                	<div class="slider-wrapper">
+                    	<div class="slide">
+                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/1.jpg">
+                        </div>
+                        <div class="slide">
+                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/2.jpg">
+                        </div>
+                        <div class="slide">
+                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/3.jpg">
+                        </div>
+                        <div class="slide">
+                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/4.jpg">
+                        </div>
+                        <div class="slide">
+                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/5.jpg">
+                        </div>
+                    </div>
+                    <div class="banner-content">
+                    	<a href="" class="button-large button-white semibold">our chrd</a>
+                        <p>Experience the uncommon element</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="clear-fixed inner-sections thumbnail-listing">
+    	<div class="fluid-container">
+        	<div class="content-row">
+            	<div class="grid-12 text-center">
+                	<h2 class="section-title text-uppercase">staying locations</h2>
+                    <p class="times-new-roman text-big text-italic">Stylish accommodation option at CHRD Resort & Spa incorporates 6 different locations across the country with a choice of 16 Deluxe Suites, 2 Deluxe Family Suite and 2 Pool Villas to anticipate all of your holiday needs. All categories are the epitome of Bangladeshi luxury with customized interiors and essential home comforts. </p>
+                </div>
+            </div>
+        	<div class="content-row">
+            	<div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img1.png);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Madhupur chrd</a>
+                    </div>
+                </div>
+                <div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img2.jpg);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Gazipur chrd</a>
+                    </div>
+                </div>
+                <div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img3.jpg);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Chittagong chrd</a>
+                    </div>
+                </div>
+                <div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img4.jpg);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Khulna chrd</a>
+                    </div>
+                </div>
+                <div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img5.jpg);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Comilla chrd</a>
+                    </div>
+                </div>
+                <div class="grid-4">
+                	<div class="image-thumbnail">
+                    	<a class="image-holder" href="" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/Madhobpur/img6.jpg);"></a>
+                        <a class="chrd-name bold text-uppercase text-center" href="">Sylhet chrd</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="clear-fixed why-choose-chrd" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/why-chrd-banner.jpg);">
+    	<div class="why-chrd-content">
+        	<h2 class="section-title white text-uppercase">why choose chrd </h2>
+            <p class="text-big dark-gray times-new-roman text-italic">Our resort provides you some of the most breathtaking view where you can be one with the nature and make your body and mind super relaxed. Our range of hospitality features provides you with the utmost royal happiness. <br> <br> We provide transport for any customer from their home location to our resort. So what are you waiting for, book a room today and Enjoy....</p>
+        </div>
+    </section>
+    <section class="clear-fixed inner-sections text-center">
+    	<div class="content-row">
+        	<div class="grid-12 inner-sections">
+            	<h2 class="section-title text-uppercase">customer feedbacks</h2>
+                
+                <div class="customer-feedbacks">
+                	<div class="feedback-slides">
+                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
+                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
+                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
+                    </div>
+                    <div class="feedback-slides">
+                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
+                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
+                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
+                    </div>
+                    <div class="feedback-slides">
+                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
+                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
+                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
+                    </div>
+                    <div class="feedback-slides">
+                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
+                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
+                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+get_footer();
