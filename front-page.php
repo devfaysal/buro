e750f9884d8c7696b8e9e2c9fd5b82bc817eef7e
Faysal Ahamed <miufaysal@live.com>
2019-02-28 19:00:14 +0600

    home page dynamic

--- front-page.php
+++ front-page.php
@@ -5,25 +5,25 @@
         	<div class="content-row">
             	<div class="grid-12">
                 	<div class="slider-wrapper">
-                    	<div class="slide">
-                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/1.jpg">
-                        </div>
-                        <div class="slide">
-                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/2.jpg">
-                        </div>
-                        <div class="slide">
-                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/3.jpg">
-                        </div>
-                        <div class="slide">
-                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/4.jpg">
-                        </div>
-                        <div class="slide">
-                        	<img src="<?php bloginfo('stylesheet_directory'); ?>/image/homepage-slider/5.jpg">
-                        </div>
+                        <?php 
+                        if( have_rows('slider') ): 
+                            // loop through the rows of data
+                            while ( have_rows('slider') ) : the_row();
+                            // display a sub field value
+                        ?>
+
+                            <div class="slide">
+                                <img src="<?php the_sub_field('image'); ?>">
+                            </div>
+
+                        <?php
+                            endwhile;
+                        endif;
+                        ?>
                     </div>
                     <div class="banner-content">
                     	<a href="" class="button-large button-white semibold">our chrd</a>
-                        <p>Experience the uncommon element</p>
+                        <p><?php the_field('slider_subtitle'); ?></p>
                     </div>
                 </div>
             </div>
@@ -33,10 +33,16 @@
     	<div class="fluid-container">
         	<div class="content-row">
             	<div class="grid-12 text-center">
-                	<h2 class="section-title text-uppercase">staying locations</h2>
-                    <p class="times-new-roman text-big text-italic">Stylish accommodation option at CHRD Resort & Spa incorporates 6 different locations across the country with a choice of 16 Deluxe Suites, 2 Deluxe Family Suite and 2 Pool Villas to anticipate all of your holiday needs. All categories are the epitome of Bangladeshi luxury with customized interiors and essential home comforts. </p>
+                	<h2 class="section-title text-uppercase"><?php the_field('heading'); ?></h2>
+                    <p class="times-new-roman text-big text-italic"><?php the_field('description'); ?></p>
                 </div>
             </div>
+            <?php
+                echo '<pre>';
+                print_r( get_field('chrds')  );
+                echo '</pre>';
+                die;
+            ?>
         	<div class="content-row">
             	<div class="grid-4">
                 	<div class="image-thumbnail">
@@ -78,10 +84,14 @@
         </div>
     </section>
     
-    <section class="clear-fixed why-choose-chrd" style="background-image:url(<?php bloginfo('stylesheet_directory'); ?>/image/image-gallery/why-chrd-banner.jpg);">
+    <section class="clear-fixed why-choose-chrd" style="background-image:url(<?php the_field('image'); ?>);">
     	<div class="why-chrd-content">
         	<h2 class="section-title white text-uppercase">why choose chrd </h2>
-            <p class="text-big dark-gray times-new-roman text-italic">Our resort provides you some of the most breathtaking view where you can be one with the nature and make your body and mind super relaxed. Our range of hospitality features provides you with the utmost royal happiness. <br> <br> We provide transport for any customer from their home location to our resort. So what are you waiting for, book a room today and Enjoy....</p>
+            <p class="text-big dark-gray times-new-roman text-italic">
+            Our resort provides you some of the most breathtaking view where you can be one with the nature and make your body and mind super relaxed. Our range of hospitality features provides you with the utmost royal happiness. 
+            <br> <br> 
+            We provide transport for any customer from their home location to our resort. So what are you waiting for, book a room today and Enjoy....
+            </p>
         </div>
     </section>
     <section class="clear-fixed inner-sections text-center">
@@ -90,26 +100,23 @@
             	<h2 class="section-title text-uppercase">customer feedbacks</h2>
                 
                 <div class="customer-feedbacks">
-                	<div class="feedback-slides">
-                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
-                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
-                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
-                    </div>
-                    <div class="feedback-slides">
-                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
-                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
-                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
-                    </div>
-                    <div class="feedback-slides">
-                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
-                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
-                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
-                    </div>
-                    <div class="feedback-slides">
-                    	<h4 class="light text-uppercase margin-zero">Jane Doe</h4>
-                        <p class="dark-gray text-big">Stayed at Gazipur CHRD at a duplex room for 2 days</p>
-                        <p class="quote times-new-roman text-italic">“I have visited many resorts across the country but none matches the quality and service of CHRD. To my opinion, when it comes to hospitality business, they are the best across the country.”</p>
-                    </div>
+                    <?php 
+                    if( have_rows('customer_feedbacks') ): 
+                        // loop through the rows of data
+                        while ( have_rows('customer_feedbacks') ) : the_row();
+                        // display a sub field value
+                    ?>
+
+                        <div class="feedback-slides">
+                    	    <h4 class="light text-uppercase margin-zero"><?php the_sub_field('name'); ?></h4>
+                            <p class="dark-gray text-big"><?php the_sub_field('which_chrd'); ?></p>
+                            <p class="quote times-new-roman text-italic">“<?php the_sub_field('feedback'); ?>”</p>
+                        </div>
+
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
                 </div>
             </div>
         </div>
